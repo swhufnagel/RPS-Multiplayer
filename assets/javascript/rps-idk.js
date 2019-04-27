60f2e419cb087267552875a75aa5839df95cb73f
Sean Huff <swhufnagel@gmail.com>
2019-04-27 02:25:28 -0600

    two page bug

--- assets/javascript/rps-idk.js
+++ assets/javascript/rps-idk.js
@@ -13,33 +13,6 @@ var scoreboard = {
   wins2: 0,
   ties: 0
 }
-var currentPlayers;
-var playerOneExists;
-var playerTwoExists;
-var playerOneData;
-var playerTwoData;
-var uid;
-var config = {
-  apiKey: "AIzaSyB08z3Po7GOR_UOFWv55Tg9gjCycdrBk3o",
-  authDomain: "multiplay-rps.firebaseapp.com",
-  databaseURL: "https://multiplay-rps.firebaseio.com",
-  projectId: "multiplay-rps",
-  storageBucket: "multiplay-rps.appspot.com",
-  messagingSenderId: "478262105305"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-var playersRef = database.ref('players');
-var user = firebase.auth().signInAnonymously();
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    // User is signed in.
-    var isAnonymous = user.isAnonymous;
-    uid = user.uid;
-  } else {
-    // User is signed out.
-  }
-});
 //functions
 function generateBtns() {
   for (var j = 0; j < 2; j++) {
@@ -50,9 +23,9 @@ function generateBtns() {
       div.addClass("choices");
       var button = $("<button>");
       if (j === 0) {
-        button.addClass("choice btn btn-outline-dark");
+        button.addClass("choice");
       }
-      else { button.addClass("choice2 btn btn-outline-dark"); }
+      else { button.addClass("choice2"); }
       button.attr("id", choices[i]);
       button.html(choices[i]);
       div.append(button);
@@ -61,46 +34,34 @@ function generateBtns() {
   }
 }
 function showGuesses() {
-  $(".player1choice").html(playerOneData.choice);
-  $(".player2choice").html(playerTwoData.choice);
+  $(".player1choice").html(playerOneData.choice)
+  $(".player2choice").html(playerTwoData.choice)
+
 }
 function checkWin() {
-  console.log(playerOneData.choice);
-  console.log(playerTwoData.choice);
-  database.ref("/players/1").set({
-    name: $(".player2").text(),
-    choice: players.player2Choice
-  });
-  database.ref("/players/0").set({
-    name: $(".player1").text(),
-    choice: players.player1Choice
-  });
-    if (playerOneData.choice === playerTwoData.choice) {
+  if (players.player1Picked === true && players.player2Picked === true) {
+    if (players.player1Choice === players.player2Choice) {
       scoreboard.ties += 1;
       $(".resultsbox").html("Tie!");
-      console.log("tie");
       showGuesses();
       playAgain();
     }
-    else if (playerOneData.choice === 'rock' && playerTwoData.choice === 'scissors' || playerOneData.choice === 'scissors' && playerTwoData.choice === 'paper' ||
-      playerOneData.choice === 'paper' && playerTwoData.choice === 'rock') {
+    else if (players.player1Choice === 'rock' && players.player2Choice === 'scissors' || players.player1Choice === 'scissors' && players.player2Choice === 'paper' ||
+      players.player1Choice === 'paper' && players.player2Choice === 'rock') {
       scoreboard.wins1 += 1;
       $(".resultsbox").html(playerOneData.name + " Wins!")
       showGuesses();
       playAgain();
-      console.log("win");
-
     }
-    else if (playerOneData.choice === 'rock' && playerTwoData.choice === 'paper' || playerOneData.choice === 'scissors' && playerTwoData.choice === 'rock' ||
-      playerOneData.choice === 'paper' && playerTwoData.choice === 'scissors') {
+    else if (players.player1Choice === 'rock' && players.player2Choice === 'paper' || players.player1Choice === 'scissors' && players.player2Choice === 'rock' ||
+      players.player1Choice === 'paper' && players.player2Choice === 'scissors') {
       scoreboard.wins2 += 1;
       $(".resultsbox").html(playerTwoData.name + " Wins!");
       showGuesses();
       playAgain();
-      console.log("loss");
-
     }
   }
+}
 function playAgain() {
   var button = $("<button>");
   button.addClass("playAgain");
@@ -120,6 +81,39 @@ function playAgain() {
   })
 }
 // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB08z3Po7GOR_UOFWv55Tg9gjCycdrBk3o",
+  authDomain: "multiplay-rps.firebaseapp.com",
+  databaseURL: "https://multiplay-rps.firebaseio.com",
+  projectId: "multiplay-rps",
+  storageBucket: "multiplay-rps.appspot.com",
+  messagingSenderId: "478262105305"
+};
+firebase.initializeApp(config);
+var user = firebase.auth().signInAnonymously();
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    var isAnonymous = user.isAnonymous;
+    uid = user.uid;
+    console.log(isAnonymous);
+    console.log(uid);
+    console.log(user);
+  } else {
+    // User is signed out.
+  }
+});
+var database = firebase.database();
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+
+var currentPlayers;
+var playerOneExists;
+var playerTwoExists;
+var playerOneData;
+var playerTwoData;
+var playersRef = database.ref('players');
+var uid;
 playersRef.on("value", function (snapshot) {
   currentPlayers = snapshot.numChildren();
 
@@ -148,13 +142,6 @@ playersRef.on("value", function (snapshot) {
 })
 $(window).on("load", function () {
   database.ref().set({});
-  var playing = false;
-  playersRef.on("value", function(){
-    if(currentPlayers=== 2 & playing === false){
-    generateBtns();
-    playing = true;
-    }
-  })
   database.ref("/chat").on("child_added", function (data) {
     var type;
     if(data.val().user_id == uid){
@@ -163,69 +150,41 @@ $(window).on("load", function () {
     else{
       type="replies";
     }
-    $('.chatbox').append("<div class ='"+type+"holder'><span class="+type+">" + data.val().message) + "</span></div>";
-  })
-  database.ref("/count").on("value", function(snapshot){
-    if(snapshot.val().players > 1){
-      $(".login").addClass('hide');
-    }
-  })
-  database.ref("/picked").on("value", function(snapshot){
-    if(snapshot.val().player1 === true){
-      $(".player1choice").empty();
-    }
-    if(snapshot.val().player2 === true){
-      $(".player2choice").empty();
-    }
-    if(snapshot.val().player1 === true && snapshot.val().player2 === true){
-      console.log("checking");
-      checkWin();
-    }
-  })
+    $('.chatbox').append("<div class="+type+">" + data.val().message) + "</div>";
+    // $('<li class="'+type+'"><p>' + data.val().message + '</p></li>').appendTo($('.messages ul'));
+    // $('.message-input input').val(null);
+    // $('.contact.active .preview').html('<span>You: </span>' + data.val().message);
+    //   $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight }, 0);
+  });
   });
   //on clicks
   //keep chat
   $(document).on("click", "#send", function () {
     event.preventDefault();
     var message = $("#message").val().trim();
-    if(message !== ""){
     database.ref("/chat").push({
       user_id: uid,
       message: message
     });
-    function ScrollDiv(){
-      if(document.getElementById('ecran').scrollTop<(document.getElementById('ecran').scrollHeight-document.getElementById('ecran').offsetHeight)){-1
-            document.getElementById('ecran').scrollTop=document.getElementById('ecran').scrollTop+1
-            }
-   }
-   setInterval(ScrollDiv,20)
     $("#message").val(null);
-  }
   })
 $(document).on("click", ".submit", function () {
   event.preventDefault();
+  console.log(currentPlayers);
   if (currentPlayers === 1) {
-    $(".login").addClass('hide');
+    $("button").remove('.login');
+    generateBtns();
   }
   if (currentPlayers >= 2) {
     alert("Error: Game is Full");
   }
   if ($(".name").val().trim() !== "" && currentPlayers < 2) {
     players.number++;
-    console.log(players.number);
-    database.ref("count/").set({
-      players: players.number
-    })
     $(".modal").removeClass("in");
     $("div").remove(".modal-backdrop");
     $(".modal").attr("style='display:block;'");
 
     if (currentPlayers > 0 && playerOneData.name === $(".name").val().trim()) {
-      players.number++;
-      database.ref("count/").set({
-        players: players.number
-      })
-      $(".login").addClass('hide');
       database.ref("players/" + currentPlayers).set({
         name: $(".name").val().trim() + "2",
         choice: ""
@@ -244,28 +203,19 @@ $(document).on("click", ".submit", function () {
 $(document).on("click", ".choice", function () {
   players.player1Choice = $(this).attr("id");
   players.player1Picked = true;
-  database.ref("/picked").set({
-    player1: true,
-    player2: players.player2Picked
-  })
   database.ref("/players/0").set({
     name: $(".player1").text(),
     choice: players.player1Choice
   });
   $(".player1choice").empty();
+  checkWin();
 })
 $(document).on("click", ".choice2", function () {
   players.player2Choice = $(this).attr("id");
   players.player2Picked = true;
-  database.ref("/picked").set({
-    player1: players.player1Picked,
-    player2: true
-  })
   database.ref("/players/1").set({
     name: $(".player2").text(),
     choice: players.player2Choice
   });
-  // $(".player2choice").empty();
-  console.log(playerOneData.choice);
-  console.log(playerTwoData.choice);
+  checkWin();
 })
